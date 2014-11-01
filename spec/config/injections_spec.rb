883c911a6ef4f871dc7fae8af53bd84ce3c9d130
spape <github@pape.io>
2014-11-01 18:08:18 +0100

    fix priority of injections

--- spec/config/injections_spec.rb
+++ spec/config/injections_spec.rb
@@ -10,6 +10,13 @@ describe LHC do
       LHC.get(':datastore/feedbacks')
     end
 
+    it 'injects explicit values first' do
+      LHC.config.injection(:campaign_id, '123')
+      stub_request(:get, 'http://datastore-stg.lb-service/v2/campaign/456/feedbacks')
+      url = 'http://datastore-stg.lb-service/v2/campaign/:campaign_id/feedbacks'
+      LHC.get(url, params: { campaign_id: '456' })
+    end
+
     it 'raises in case of claching injection name' do
       LHC.config.injection(:datastore, 'http://datastore.lb-service')
       expect(->{
