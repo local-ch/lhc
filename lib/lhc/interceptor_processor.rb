cab5c7cdc5533a5a427fff926a4bcff5c7a911bb
spape <github@pape.io>
2014-11-02 11:03:46 +0100

    [DOCs] some text for intercetpor processor

--- lib/lhc/interceptor_processor.rb
+++ lib/lhc/interceptor_processor.rb
@@ -1,13 +1,16 @@
+# Handles interceptions during the lifecycle of a request
 class LHC::InterceptorProcessor
 
   attr_accessor :interceptors
 
+  # Intitalizes the processor and determines if global or local interceptors are used
   def initialize(target)
     options = target.options if target.is_a? LHC::Request
     options ||= target.request.options if target.is_a? LHC::Response
     self.interceptors = (options[:interceptors] || LHC.config.interceptors).map{ |i| i.new }
   end
 
+  # Forwards messages to interceptors and handles injected responses.
   def intercept(name, target)
     interceptors.each do |interceptor|
       result = interceptor.send(name, target)
