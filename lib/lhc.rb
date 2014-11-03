16d1cb44f43ba2bed66b6831d571102da5876c3c
spape <github@pape.io>
2014-11-03 15:48:52 +0100

    complete concern is overhead

--- lib/lhc.rb
+++ lib/lhc.rb
@@ -2,7 +2,11 @@ Dir[File.dirname(__FILE__) + '/lhc/concerns/lhc/*.rb'].each {|file| require file
 
 module LHC
   include BasicMethods
-  include Configuration
+
+  def self.config
+    LHC::Config.instance
+  end
 end
+
 
 Gem.find_files('lhc/**/*.rb').each { |path| require path }
