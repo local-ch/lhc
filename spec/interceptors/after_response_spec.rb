d7f3f8e608ff8e16d93749841a21a97c1f2e8828
spape <github@pape.io>
2015-09-28 10:19:24 +0200

    + spec/interceptors/after_response_spec.rb after cleanup

---
+++ spec/interceptors/after_response_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+describe LHC do
+
+  context 'interceptor' do
+
+    before(:each) do
+      class Services
+        def self.timing(path, time)
+        end
+      end
+    end
+
+    before(:each) do
+      class StatsTimingInterceptor < LHC::Interceptor
+        def after_response(response)
+          uri = URI.parse(response.request.url)
+          path = [
+            'web',
+            Rails.application.class.parent_name,
+            Rails.env,
+            response.request.method,
+            uri.scheme,
+            uri.host,
+            response.code
+          ].join('.')
+          Services.timing(path.downcase, response.time)
+        end
+      end
+      LHC.configure { |c| c.interceptors = [StatsTimingInterceptor] }
+    end
+
+    let(:url) { "http://local.ch/v2/feedbacks/-Sc4_pYNpqfsudzhtivfkA" }
+
+    it 'can take action after a response was received' do
+      allow(Services).to receive(:timing).with('web.dummy.test.get.http.local.ch.200', 0)
+      stub_request(:get, url)
+      LHC.get(url)
+      expect(Services).to have_received(:timing)
+    end
+  end
+end
