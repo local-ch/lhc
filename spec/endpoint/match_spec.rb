924023fdac0b585501110359b706cb30d60b30f3
spape <github@pape.io>
2015-10-01 13:44:35 +0200

    ! improve endpoint matcher

--- spec/endpoint/match_spec.rb
+++ spec/endpoint/match_spec.rb
@@ -7,13 +7,13 @@ describe LHC::Endpoint do
     context 'matching' do
       it 'checks if a url matches a template' do
         {
-          ':datastore/v2/places' => 'http://silo1.intra.local.ch:8082/v2/places',
-          ':datastore/v2/places/:id' => 'http://silo1.intra.local.ch:8082/v2/places/ZW9OJyrbt4OZE9ueu80w-A',
-          ':datastore/v2/places/:namespace/:id' => 'http://silo1.intra.local.ch:8082/v2/places/switzerland/ZW9OJyrbt'
+          ':datastore/v2/places' => 'http://local.ch:8082/v2/places',
+          ':datastore/v2/places/:id' => 'http://local.ch:8082/v2/places/ZW9OJyrbt4OZE9ueu80w-A',
+          ':datastore/v2/places/:namespace/:id' => 'http://local.ch:8082/v2/places/switzerland/ZW9OJyrbt'
         }.each do |template, url|
           expect(
             LHC::Endpoint.match?(url, template)
-          ).to be
+          ).to be, "#{url} should match #{template}!"
         end
       end
     end
@@ -21,11 +21,12 @@ describe LHC::Endpoint do
     context 'not matching' do
         it 'checks if a url matches a template' do
           {
-            ':datastore/v2/places' => 'http://silo1.intra.local.ch:8082/v2/places/ZW9OJyrbt4OZE9ueu80w-A'
+            ':datastore/v2/places' => 'http://local.ch:8082/v2/places/ZW9OJyrbt4OZE9ueu80w-A',
+            ':datastore/:campaign_id/feedbacks' => 'http://datastore.local.ch/feedbacks'
           }.each do |template, url|
             expect(
               LHC::Endpoint.match?(url, template)
-            ).not_to be
+            ).not_to be, "#{url} should not match #{template}!"
           end
         end
     end
