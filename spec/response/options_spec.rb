06b6546d4a1361cf44e3e2d9b1d72d7fdda3dc40
spape <github@pape.io>
2015-10-28 14:12:09 +0100

    make response options accessible

---
+++ spec/response/options_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe LHC::Response do
+
+  context 'headers' do
+
+    let(:options) do
+      { 'return_code' => :ok }
+    end
+
+    let(:raw_response) { OpenStruct.new({ options: options }) }
+
+    it 'provides headers' do
+      response = LHC::Response.new(raw_response, nil)
+      expect(response.options).to eq options
+    end
+  end
+end
