af0e025f954ddfb45d404872a4b30640485e0b57
spape <github@pape.io>
2015-11-27 10:18:06 +0100

    ~ cleanup topology

--- spec/endpoint/values_as_params_spec.rb
+++ spec/endpoint/values_as_params_spec.rb
@@ -6,15 +6,15 @@ describe LHC::Endpoint do
 
     it 'provides params extracting values from a provided url and template' do
       [
-        [':datastore/v2/places', 'http://silo1.intra.local.ch:8082/v2/places', {
-          datastore: 'http://silo1.intra.local.ch:8082'
+        [':datastore/v2/places', 'http://local.ch:8082/v2/places', {
+          datastore: 'http://local.ch:8082'
         }],
-        [':datastore/v2/places/:id', 'http://silo1.intra.local.ch:8082/v2/places/ZW9OJyrbt4OZE9ueu80w-A', {
-          datastore: 'http://silo1.intra.local.ch:8082',
+        [':datastore/v2/places/:id', 'http://local.ch:8082/v2/places/ZW9OJyrbt4OZE9ueu80w-A', {
+          datastore: 'http://local.ch:8082',
           id: 'ZW9OJyrbt4OZE9ueu80w-A'
         }],
-        [':datastore/v2/places/:namespace/:id', 'http://silo1.intra.local.ch:8082/v2/places/switzerland/ZW9OJyrbt', {
-          datastore: 'http://silo1.intra.local.ch:8082',
+        [':datastore/v2/places/:namespace/:id', 'http://local.ch:8082/v2/places/switzerland/ZW9OJyrbt', {
+          datastore: 'http://local.ch:8082',
           namespace: 'switzerland',
           id: 'ZW9OJyrbt'
         }],
