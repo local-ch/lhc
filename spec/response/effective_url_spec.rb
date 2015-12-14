3d1b4c2b293eed391577a46cfdcf21b9a450d82f
spape <github@pape.io>
2015-12-14 09:36:48 +0100

    ! fix confusion!

--- spec/response/effective_url_spec.rb
+++ spec/response/effective_url_spec.rb
@@ -4,11 +4,9 @@ describe LHC::Response do
 
   context 'effective_url' do
 
-    let(:effective_url) do
-      { 'effective_url' => 'https://www.local.ch' }
-    end
+    let(:effective_url) { 'https://www.local.ch' }
 
-    let(:raw_response) { OpenStruct.new({ effective_url: effective_url }) }
+    let(:raw_response) { OpenStruct.new(effective_url: effective_url) }
 
     it 'provides effective_url' do
       response = LHC::Response.new(raw_response, nil)
