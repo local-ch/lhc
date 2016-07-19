66c91c863136392a99cc07995c292d9693f911ee
spape <s@pape.io>
2016-07-19 09:41:16 +0200

    ~ less strict placeholder regexp

--- spec/endpoint/compile_spec.rb
+++ spec/endpoint/compile_spec.rb
@@ -26,5 +26,12 @@ describe LHC::Endpoint do
         endpoint.compile(datastore: 'http://datastore', id: 123)
       ).to eq "http://datastore/entries/123.json"
     end
+
+    it 'compiles complex urls containing all sort of characters' do
+      endpoint = described_class.new(':ads/?adrawdata/3.0/1108.1/2844859/0/0/header=yes;cookie=no;adct=204;alias=:region_id:lang:product_type:product;key=cat=:category_id')
+      expect(
+        endpoint.compile(ads: 'http://ads', region_id: 291, lang: 'de', product_type: 'ABC', product: 'xyz', category_id: 312)
+      ).to eq 'http://ads/?adrawdata/3.0/1108.1/2844859/0/0/header=yes;cookie=no;adct=204;alias=291deABCxyz;key=cat=312'
+    end
   end
 end
