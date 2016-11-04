2ebe6145666f78775b1c86ecca56d0ea8dc3ade2
spape <s@pape.io>
2016-11-04 16:34:15 +0100

    + access data directly on response

---
+++ spec/response/data_accessor_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe LHC do
+  context 'data accessor' do
+    it 'makes data accessible with square bracket accessor' do
+      stub_request(:get, "http://local.ch/")
+        .with(headers: { 'Accept' => 'application/json', 'Content-Type' => 'application/json' })
+        .to_return(body: {'MyProp' => 'MyValue'}.to_json)
+      expect(
+        LHC.json.get('http://local.ch')['MyProp']
+      ).to eq 'MyValue'
+    end
+  end
+end
