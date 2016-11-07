15362de0a45b48f08b0d0e8247099ce7450febf8
spape <s@pape.io>
2016-11-07 11:12:24 +0100

    + spec for accessing hash with indifferent access: string and symbol

--- spec/response/data_accessor_spec.rb
+++ spec/response/data_accessor_spec.rb
@@ -1,13 +1,23 @@
 require 'rails_helper'
 
 describe LHC do
-  context 'data accessor' do
-    it 'makes data accessible with square bracket accessor' do
+  context 'data accessor (hash with indifferent access)' do
+
+    before(:each) do
       stub_request(:get, "http://local.ch/")
         .with(headers: { 'Accept' => 'application/json', 'Content-Type' => 'application/json' })
         .to_return(body: { 'MyProp' => 'MyValue' }.to_json)
+    end
+
+    it 'makes data accessible with square bracket accessor (string)' do
       expect(
         LHC.json.get('http://local.ch')['MyProp']
+      ).to eq 'MyValue'
+    end
+
+    it 'makes data accessible with square bracket accessor (symbol)' do
+      expect(
+        LHC.json.get('http://local.ch')[:MyProp]
       ).to eq 'MyValue'
     end
   end
