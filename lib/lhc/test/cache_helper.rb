c1b7211310622119f77d7063bec56c173fce5a6a
spape <s@pape.io>
2017-04-13 10:44:25 +0200

    ! dont configure rspec when its not arround

--- lib/lhc/test/cache_helper.rb
+++ lib/lhc/test/cache_helper.rb
@@ -1,7 +1,9 @@
-RSpec.configure do |config|
-  LHC::Caching.cache = ActiveSupport::Cache::MemoryStore.new
+if defined?(RSpec)
+  RSpec.configure do |config|
+    LHC::Caching.cache = ActiveSupport::Cache::MemoryStore.new
 
-  config.before(:each) do
-    LHC::Caching.cache.clear
+    config.before(:each) do
+      LHC::Caching.cache.clear
+    end
   end
 end
