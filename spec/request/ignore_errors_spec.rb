582ef5511615b93337ee42c67d6193624a043b62
spape <s@pape.io>
2017-08-16 12:55:21 +0200

    ~ wording + test for not inheriting error

--- spec/request/ignore_errors_spec.rb
+++ spec/request/ignore_errors_spec.rb
@@ -24,11 +24,18 @@ describe LHC::Request do
   end
 
   context 'inheritance when ignoring errors' do
-    subject { LHC.get('http://local.ch', ignored_errors: [LHC::Error]) }
     before { stub_request(:get, 'http://local.ch').to_return(status: 404) }
 
-    it 'does not raise an error when its a subclass of the ignored error' do
-      expect { subject }.not_to raise_error
+    it "does not raise an error when it's a subclass of the ignored error" do
+      expect {
+        LHC.get('http://local.ch', ignored_errors: [LHC::Error])
+      }.not_to raise_error
+    end
+
+    it "does raise an error if it's not a subclass of the ignored error" do
+      expect {
+        LHC.get('http://local.ch', ignored_errors: [ArgumentError])
+      }.to raise_error(LHC::NotFound)
     end
   end
 end
