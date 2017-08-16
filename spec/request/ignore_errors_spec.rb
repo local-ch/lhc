acf3af85f092fe0ae90f35ea647670e0faf83b08
spape <s@pape.io>
2017-08-16 11:38:45 +0200

    + take in considarition error inheritance

--- spec/request/ignore_errors_spec.rb
+++ spec/request/ignore_errors_spec.rb
@@ -22,4 +22,13 @@ describe LHC::Request do
       expect { subject }.to raise_error LHC::InternalServerError
     end
   end
+
+  context 'inheritance when ignoring errors' do
+    subject { LHC.get('http://local.ch', ignored_errors: [LHC::Error]) }
+    before { stub_request(:get, 'http://local.ch').to_return(status: 404) }
+
+    it 'does not raise an error when its a subclass of the ignored error' do
+      expect { subject }.not_to raise_error
+    end
+  end
 end
