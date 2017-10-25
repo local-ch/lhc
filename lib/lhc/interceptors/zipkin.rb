77c1c2b12135e94a97ffad7082db3e9d737c286c
Sascha Kaestle <sascha.kaestle@gmail.com>
2017-10-25 17:12:07 +0200

    add zipkin interceptor

---
+++ lib/lhc/interceptors/zipkin.rb
@@ -0,0 +1,11 @@
+class LHC::Zipkin < LHC::Interceptor
+
+  def before_request(request)
+    return unless defined?(ZipkinTracer::TraceContainer)
+    container = ZipkinTracer::TraceContainer.current
+    request.headers['X-B3-TraceId'] = container.trace_id.to_s
+    request.headers['X-B3-ParentSpanId'] = container.parent_id.to_s if container.parent_id
+    request.headers['X-B3-Sampled'] = container.sampled
+    request.headers['X-B3-Flags'] = container.flags
+  end
+end
