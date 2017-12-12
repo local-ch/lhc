b29387a949e23d2d78b89be393a7880aaca26c52
Franca Rast <franca.rast@local.ch>
2017-12-12 14:27:31 +0100

    ~ shared instead of copy paste

--- spec/basic_methods/request_spec.rb
+++ spec/basic_methods/request_spec.rb
@@ -2,16 +2,6 @@ require 'rails_helper'
 
 describe LHC do
   context 'request' do
-    let(:total) { 99 }
-
-    before(:each) do
-      stub_request(:get, "http://datastore/v2/feedbacks?has_reviews=true")
-        .to_return(status: 200, body: { total: total }.to_json, headers: { 'Content-Encoding' => 'UTF-8' })
-    end
-
-    it 'does a request returning a response' do
-      response = LHC.request(url: 'http://datastore/v2/feedbacks', params: { has_reviews: true }, method: :get)
-      expect(response.data.total).to eq total
-    end
+    include_examples 'basic methods request'
   end
 end
