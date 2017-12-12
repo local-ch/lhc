f89b908746e77781bb277da7140f3d259123715a
Franca Rast <franca.rast@local.ch>
2017-12-12 15:08:20 +0100

    ~ run that cop

--- spec/basic_methods/request_without_rails_spec.rb
+++ spec/basic_methods/request_without_rails_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe LHC do
   context 'request' do
     it "is able to call .request without LHC raising NoMethodError: undefined method `blank?' for nil:NilClass when calling it outside of the rails context" do
-      expect{ LHC.request(url: "http://datastore/v2/feedbacks", method: :get) }.to_not raise_error(NoMethodError)
+      expect { LHC.request(url: "http://datastore/v2/feedbacks", method: :get) }.to_not raise_error(NoMethodError)
     end
   end
 end
