9ae1066b2b4ebd1b6796c67ac547e71f3f371ea7
Franca Rast <franca.rast@local.ch>
2017-12-12 15:06:07 +0100

    ~ rewrite spec to be clearer what the spec is about

--- spec/basic_methods/request_without_rails_spec.rb
+++ spec/basic_methods/request_without_rails_spec.rb
@@ -2,16 +2,8 @@ require 'spec_helper'
 
 describe LHC do
   context 'request' do
-    let(:total) { 99 }
-
-    before(:each) do
-      stub_request(:get, "http://datastore/v2/feedbacks?has_reviews=true")
-        .to_return(status: 200, body: { total: total }.to_json, headers: { 'Content-Encoding' => 'UTF-8' })
-    end
-
-    it 'does a request returning a response' do
-      response = LHC.request(url: 'http://datastore/v2/feedbacks', params: { has_reviews: true }, method: :get)
-      expect(response.data.total).to eq total
+    it "is able to call .request without LHC raising NoMethodError: undefined method `blank?' for nil:NilClass when calling it outside of the rails context" do
+      expect{ LHC.request(url: "http://datastore/v2/feedbacks", method: :get) }.to_not raise_error(NoMethodError)
     end
   end
 end
