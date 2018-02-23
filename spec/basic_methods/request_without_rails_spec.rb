799e5faa93d32ee57854aae01bd26b5acd2f163d
Sascha Kaestle <sascha.kaestle@gmail.com>
2018-02-23 12:02:42 +0100

    fix raise error check to not hide any errors

--- spec/basic_methods/request_without_rails_spec.rb
+++ spec/basic_methods/request_without_rails_spec.rb
@@ -2,8 +2,9 @@ require 'spec_helper'
 
 describe LHC do
   context 'request' do
+    before { stub_request(:get, "http://datastore/v2/feedbacks").to_return(status: 200, body: "{}") }
     it "is able to call .request without LHC raising NoMethodError: undefined method `blank?' for nil:NilClass when calling it outside of the rails context" do
-      expect { LHC.request(url: "http://datastore/v2/feedbacks", method: :get) }.not_to raise_error(NoMethodError)
+      expect { LHC.request(url: "http://datastore/v2/feedbacks", method: :get) }.not_to raise_error
     end
   end
 end
