1cedd730591d7bec1ab233e266d40d0e14432e8f
skaestle <sascha.kaestle@gmail.com>
2018-03-07 09:43:58 +0100

    add require for deep_symbolize_keys (#118)
    ! add require for deep_symbolize_keys

--- spec/basic_methods/request_without_rails_spec.rb
+++ spec/basic_methods/request_without_rails_spec.rb
@@ -1,9 +1,28 @@
 require 'spec_helper'
 
 describe LHC do
-  context 'request' do
+  context 'GET' do
+    before do
+      stub_request(:get, "http://datastore/v2/feedbacks").to_return(status: 200, body: "{}")
+    end
     it "is able to call .request without LHC raising NoMethodError: undefined method `blank?' for nil:NilClass when calling it outside of the rails context" do
-      expect { LHC.request(url: "http://datastore/v2/feedbacks", method: :get) }.not_to raise_error(NoMethodError)
+      expect { LHC.request(url: "http://datastore/v2/feedbacks", method: :get) }.not_to raise_error
+    end
+  end
+
+  context 'POST' do
+    before do
+      stub_request(:post, "http://datastore/v2/feedbacks").to_return(status: 200, body: "{}")
+    end
+
+    it "is able to call .request without LHC raising NoMethodError: undefined method `deep_symbolize_keys' for {}:Hash" do
+      options = {
+        url: "http://datastore/v2/feedbacks",
+        method: :post,
+        body: "{}",
+        headers: { 'Content-Type' => 'application/json' }
+      }
+      expect { LHC.request(options) }.not_to raise_error
     end
   end
 end
