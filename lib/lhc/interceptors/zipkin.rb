ddcbe6b7cc92a0902ee35a9a628c91615dcb2544
Sascha Kaestle <sascha.kaestle@gmail.com>
2018-03-12 09:42:06 +0100

    memoize all the things...

--- lib/lhc/interceptors/zipkin.rb
+++ lib/lhc/interceptors/zipkin.rb
@@ -1,72 +1,68 @@
 class LHC::Zipkin < LHC::Interceptor
-
   def before_request
     return unless dependencies?
-    trace_id = ZipkinTracer::TraceGenerator.new.next_trace_id
     ZipkinTracer::TraceContainer.with_trace_id(trace_id) do
-      b3_headers.each do |method, header|
+      B3_HEADERS.each do |method, header|
         request.headers[header] = trace_id.send(method).to_s
       end
-      trace!(request, trace_id) if ::Trace.tracer && trace_id.sampled?
+      start_trace! if ::Trace.tracer && trace_id.sampled?
     end
   end
 
   def after_response
     return unless dependencies?
-    span = response.request.interceptor_environment[:zipkin_span]
-    if span
-      record_response_tags(span, response)
-    end
-    span.record(::Trace::Annotation::CLIENT_RECV, local_endpoint)
-    ::Trace.tracer.end_span(span)
+    end_trace!
   end
 
   private
 
-  SERVER_ADDRESS_SPECIAL_VALUE = '1'.freeze
+  B3_HEADERS = {
+    trace_id: 'X-B3-TraceId'.freeze,
+    parent_id: 'X-B3-ParentSpanId'.freeze,
+    span_id: 'X-B3-SpanId'.freeze,
+    sampled: 'X-B3-Sampled'.freeze,
+    flags: 'X-B3-Flags'.freeze
+  }.freeze
 
-  def b3_headers
-    {
-      trace_id: 'X-B3-TraceId',
-      parent_id: 'X-B3-ParentSpanId',
-      span_id: 'X-B3-SpanId',
-      sampled: 'X-B3-Sampled',
-      flags: 'X-B3-Flags'
-    }
+  TRUE = '1'.freeze # true in binary annotation
+
+  def trace_id
+    @trace_id ||= ZipkinTracer::TraceGenerator.new.next_trace_id
   end
 
-  def trace!(request, trace_id)
-    url = URI(request.raw.url)
+  def span
+    @span ||= ::Trace.tracer.start_span(trace_id, url.path)
+  end
+
+  def url
+    @url ||= URI(request.raw.url)
+  end
+
+  def start_trace!
     service_name = url.host
-    span = ::Trace.tracer.start_span(trace_id, url.path)
-    # annotate with method (GET/POST/etc.) and uri path
     span.record_tag(::Trace::BinaryAnnotation::PATH, url.path, ::Trace::BinaryAnnotation::Type::STRING, local_endpoint)
-    span.record_tag(::Trace::BinaryAnnotation::SERVER_ADDRESS, SERVER_ADDRESS_SPECIAL_VALUE, ::Trace::BinaryAnnotation::Type::BOOL, remote_endpoint(url, service_name))
+    span.record_tag(::Trace::BinaryAnnotation::SERVER_ADDRESS, TRUE, ::Trace::BinaryAnnotation::Type::BOOL, remote_endpoint(url, service_name))
     span.record(::Trace::Annotation::CLIENT_SEND, local_endpoint)
-    # store the span in the datum hash so it can be used in the response_call
-    request.interceptor_environment[:zipkin_span] = span
-  # rubocop:disable Lint/HandleExceptions
-  rescue ArgumentError, URI::Error
-    # Ignore URI errors, don't trace if there is no URI
-  end
-  # rubocop:enable Lint/HandleExceptions
-
-  def local_endpoint
-    # The rack middleware set this up for us.
-    ::Trace.default_endpoint
   end
 
-  def remote_endpoint(url, service_name)
-    # The endpoint we are calling.
-    ::Trace::Endpoint.remote_endpoint(url, service_name, local_endpoint.ip_format)
-  end
-
-  def record_response_tags(span, response)
+  def end_trace!
     status = response.code.to_s
     span.record_tag(::Trace::BinaryAnnotation::STATUS, status, ::Trace::BinaryAnnotation::Type::STRING, local_endpoint)
     if !response.success?
       span.record_tag(::Trace::BinaryAnnotation::ERROR, status, ::Trace::BinaryAnnotation::Type::STRING, local_endpoint)
     end
+    span.record(::Trace::Annotation::CLIENT_RECV, local_endpoint)
+    ::Trace.tracer.end_span(span)
+  end
+
+  def local_endpoint
+    # The rack middleware set this up for us.
+    @local_endpoint ||= ::Trace.default_endpoint
+  end
+
+  def remote_endpoint(url, service_name)
+    # The endpoint we are calling.
+    @remote_endpoint ||= ::Trace::Endpoint.remote_endpoint(url, service_name, local_endpoint.ip_format)
   end
 
   def dependencies?
