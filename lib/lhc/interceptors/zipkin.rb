f06e0dc04209e1ed9aa6439505904ebd34500230
Sascha Kaestle <sascha.kaestle@gmail.com>
2018-03-12 09:59:20 +0100

    object oriented approach

--- lib/lhc/interceptors/zipkin.rb
+++ lib/lhc/interceptors/zipkin.rb
@@ -16,6 +16,18 @@ class LHC::Zipkin < LHC::Interceptor
 
   private
 
+  def start_trace!
+    record_path
+    record_remote_endpoint
+    record_local_endpoint
+  end
+
+  def end_trace!
+    record_status
+    record_error if !response.success?
+    record_end
+  end
+
   B3_HEADERS = {
     trace_id: 'X-B3-TraceId'.freeze,
     parent_id: 'X-B3-ParentSpanId'.freeze,
@@ -38,30 +50,44 @@ class LHC::Zipkin < LHC::Interceptor
     @url ||= URI(request.raw.url)
   end
 
-  def start_trace!
-    service_name = url.host
-    span.record_tag(::Trace::BinaryAnnotation::PATH, url.path, ::Trace::BinaryAnnotation::Type::STRING, local_endpoint)
-    span.record_tag(::Trace::BinaryAnnotation::SERVER_ADDRESS, TRUE, ::Trace::BinaryAnnotation::Type::BOOL, remote_endpoint(url, service_name))
+  def status
+    @status ||= response.code.to_s
+  end
+
+  def service_name
+    @service_name ||= url.host
+  end
+
+  def record_local_endpoint
     span.record(::Trace::Annotation::CLIENT_SEND, local_endpoint)
   end
 
-  def end_trace!
-    status = response.code.to_s
-    span.record_tag(::Trace::BinaryAnnotation::STATUS, status, ::Trace::BinaryAnnotation::Type::STRING, local_endpoint)
-    if !response.success?
-      span.record_tag(::Trace::BinaryAnnotation::ERROR, status, ::Trace::BinaryAnnotation::Type::STRING, local_endpoint)
-    end
+  def record_remote_endpoint
+    span.record_tag(::Trace::BinaryAnnotation::SERVER_ADDRESS, TRUE, ::Trace::BinaryAnnotation::Type::BOOL, remote_endpoint)
+  end
+
+  def record_path
+    span.record_tag(::Trace::BinaryAnnotation::PATH, url.path, ::Trace::BinaryAnnotation::Type::STRING, local_endpoint)
+  end
+
+  def record_end
     span.record(::Trace::Annotation::CLIENT_RECV, local_endpoint)
     ::Trace.tracer.end_span(span)
   end
 
+  def record_error
+    span.record_tag(::Trace::BinaryAnnotation::ERROR, status, ::Trace::BinaryAnnotation::Type::STRING, local_endpoint)
+  end
+
+  def record_status
+    span.record_tag(::Trace::BinaryAnnotation::STATUS, status, ::Trace::BinaryAnnotation::Type::STRING, local_endpoint)
+  end
+
   def local_endpoint
-    # The rack middleware set this up for us.
     @local_endpoint ||= ::Trace.default_endpoint
   end
 
-  def remote_endpoint(url, service_name)
-    # The endpoint we are calling.
+  def remote_endpoint
     @remote_endpoint ||= ::Trace::Endpoint.remote_endpoint(url, service_name, local_endpoint.ip_format)
   end
 
