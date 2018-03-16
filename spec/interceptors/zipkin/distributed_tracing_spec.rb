02643a99de06868f47a5ff1872c340bc6d2604f7
Sascha Kaestle <sascha.kaestle@gmail.com>
2018-03-16 11:52:50 +0100

    fix wrong span registration (add test)

--- spec/interceptors/zipkin/distributed_tracing_spec.rb
+++ spec/interceptors/zipkin/distributed_tracing_spec.rb
@@ -13,7 +13,7 @@ describe LHC::Zipkin do
         trace_id: 'trace_id',
         parent_id: 'parent_id',
         span_id: 'span_id',
-        sampled: 'sampled',
+        sampled: true,
         flags: 'flags'
       )
     end
@@ -23,7 +23,7 @@ describe LHC::Zipkin do
       expect(headers['X-B3-TraceId']).to eq('trace_id')
       expect(headers['X-B3-ParentSpanId']).to eq('parent_id')
       expect(headers['X-B3-SpanId']).to eq('span_id')
-      expect(headers['X-B3-Sampled']).to eq('sampled')
+      expect(headers['X-B3-Sampled']).to eq('true')
       expect(headers['X-B3-Flags']).to eq('flags')
     end
   end
@@ -47,6 +47,62 @@ describe LHC::Zipkin do
       expect(headers['X-B3-SpanId']).to be_nil
       expect(headers['X-B3-Sampled']).to be_nil
       expect(headers['X-B3-Flags']).to be_nil
+    end
+  end
+
+  describe 'creating new spans' do
+    context 'sampled? is false' do
+      before(:all) do
+        ::ZipkinTracer::TraceContainer.setup_mock(
+          trace_id: 'trace_id',
+          parent_id: 'parent_id',
+          span_id: 'span_id',
+          sampled: false,
+          flags: 'flags'
+        )
+      end
+
+      it 'adds the proper X-B3 headers' do
+        headers = LHC.get(:local).request.headers
+        expect(headers['X-B3-TraceId']).to eq('trace_id')
+        expect(headers['X-B3-ParentSpanId']).to eq('parent_id')
+        expect(headers['X-B3-SpanId']).to eq('span_id')
+        expect(headers['X-B3-Sampled']).to eq('false')
+        expect(headers['X-B3-Flags']).to eq('flags')
+      end
+
+      it 'does not register a new span' do
+        # ensure no span was registered, by checking no call on span
+        expect_any_instance_of(described_class).not_to receive(:span).and_call_original
+        LHC.get(:local)
+      end
+    end
+
+    context 'sampled? is true' do
+      before(:all) do
+        ::ZipkinTracer::TraceContainer.setup_mock(
+          trace_id: 'trace_id',
+          parent_id: 'parent_id',
+          span_id: 'span_id',
+          sampled: true,
+          flags: 'flags'
+        )
+      end
+
+      it 'adds the proper X-B3 headers' do
+        headers = LHC.get(:local).request.headers
+        expect(headers['X-B3-TraceId']).to eq('trace_id')
+        expect(headers['X-B3-ParentSpanId']).to eq('parent_id')
+        expect(headers['X-B3-SpanId']).to eq('span_id')
+        expect(headers['X-B3-Sampled']).to eq('true')
+        expect(headers['X-B3-Flags']).to eq('flags')
+      end
+
+      it 'does register a new span' do
+        # ensure a span was registered, by checking call on span
+        expect_any_instance_of(described_class).to receive(:span).at_least(1).times.and_call_original
+        LHC.get(:local)
+      end
     end
   end
 end
