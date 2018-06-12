0fd96dfdb1de2247ad56f9ca922b2ab5b27571fa
Sascha Kaestle <sascha.kaestle@gmail.com>
2018-06-12 12:55:51 +0200

    work out better structure/refine approach

--- lib/lhc/interceptors/auth.rb
+++ lib/lhc/interceptors/auth.rb
@@ -1,67 +1,100 @@
 class LHC::Auth < LHC::Interceptor
   include ActiveSupport::Configurable
-  config_accessor :refresh_client_token, :retry_on
+  config_accessor :refresh_client_token, :max_recovery_attempts
 
   def before_request
-    options = request.options[:auth] || {}
-    authenticate!(request, options)
+    authenticate!
   end
 
   def after_response
-    binding.pry
-    return unless handle_refresh_and_retry?(response)
-    # refresh the token
-    token = refresh_client_token_option(response.request).call
-    set_authorization_header response.request, "Bearer #{token}"
-    # set retry on the request
-    response.request.options[:retry] = true
-    # do not try again
-    options = request.options[:auth] || {}
-    options[:was_retried_auth] = true
-    response.request.options[:auth] = options
+    return unless configuration_correct?
+    return unless attempt_recovery?
+    attempt_recovery
   end
 
   private
 
-  def authenticate!(request, options)
-    if options[:bearer]
-      bearer_authentication!(request, options)
-    elsif options[:basic]
-      basic_authentication!(request, options)
+  def authenticate!
+    if auth_options[:bearer]
+      bearer_authentication!
+    elsif auth_options[:basic]
+      basic_authentication!
     end
   end
 
-  def basic_authentication!(request, options)
-    auth = options[:basic]
+  def basic_authentication!
+    auth = auth_options[:basic]
     credentials = "#{auth[:username]}:#{auth[:password]}"
-    set_authorization_header request, "Basic #{Base64.encode64(credentials).chomp}"
+    set_authorization_header("Basic #{Base64.encode64(credentials).chomp}")
   end
 
-  def bearer_authentication!(request, options)
-    token = options[:bearer]
+  def bearer_authentication!
+    token = auth_options[:bearer]
     token = token.call if token.is_a?(Proc)
-    set_authorization_header request, "Bearer #{token}"
+    set_bearer_authorization_header(token)
   end
 
-  def set_authorization_header(request, value)
+  def set_authorization_header(value)
     request.headers['Authorization'] = value
   end
 
-  def handle_refresh_and_retry?(response)
-    options = request.options[:auth] || {}
-
-    has_bearer_header = response.request.headers['Authorization'] =~ /^Bearer [0-9a-f-]+$/i
-    retry_on = retry_on_option(response.request)
-    refresh_client_token = refresh_client_token_option(response.request)
-
-    !response.success? && has_bearer_header && !options[:was_retried_auth] && LHC::Error.find(response) == retry_on && refresh_client_token && refresh_client_token.is_a?(Proc)
+  def set_bearer_authorization_header(token)
+    set_authorization_header("Bearer #{token}")
   end
 
-  def refresh_client_token_option(request)
-    @refresh_client_token_option ||= request.options.dig(:auth, :refresh_client_token) || refresh_client_token
+  def attempt_recovery
+    # refresh token and update header
+    token = refresh_client_token_option.call
+    set_bearer_authorization_header(token)
+    # write state into request options hash - trigger LHC::Retry once - count recovery_attempts
+    request_options = request.options.dup
+    request_options[:auth] ||= {}
+    request_options[:retry] = { max: 1 }
+    request.options = request_options
+    request_options[:auth][:recovery_attempts] ||= 0
+    request_options[:auth][:recovery_attempts] += 1
+    request.options = request_options
   end
 
-  def retry_on_option(request)
-    @retry_on_option ||= request.options.dig(:auth, :retry_on) || retry_on
+  def attempt_recovery?
+    !response.success? &&
+      !(auth_options[:recovery_attempts]||0 < max_recovery_attempts_option) &&
+      bearer_header_present? &&
+      LHC::Error.find(response) == LHC::Unauthorized
+  end
+
+  def bearer_header_present?
+    @has_bearer_header ||= request.headers['Authorization'] =~ /^Bearer [0-9a-f-]+$/i
+  end
+
+  def refresh_client_token_option
+    @refresh_client_token_option ||= auth_options[:refresh_client_token] || refresh_client_token
+  end
+
+  def max_recovery_attempts_option
+    @max_recovery_attempts ||= auth_options[:max_recovery_attempts] || max_recovery_attempts || 0
+  end
+
+  def all_interceptor_classes
+    @all_interceptors ||= LHC::Interceptors.new(request).all.map(&:class)
+  end
+
+  def auth_options
+    @auth_options ||= request.options[:auth].dup || {}
+  end
+
+  def configuration_correct?
+    # only check the configuration if we got the request to attempt a recovery
+    issues = []
+    if (max_recovery_attempts_option >= 1)
+      unless refresh_client_token_option.is_a?(Proc)
+        issues << "the given refresh_client_token is either not set or not a Proc"
+      end
+      unless all_interceptor_classes.include?(LHC::Retry) && all_interceptor_classes.index(LHC::Retry) > all_interceptor_classes.index(self.class)
+        issues << "your interceptor chain needs to include LHC::Retry after LHC::Auth"
+      end
+      warn("[WARNING] Check the configuration for LHC::Auth interceptor, it's misconfigured for a retry attempt: #{issues.join('|')}") unless issues.empty?
+    end
+    issues.empty?
   end
 end
