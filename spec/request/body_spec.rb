96dce8bf8ec0a767d7a1ee12ab9badf397fae7ea
Sebastian Pape <spape@users.noreply.github.com>
2018-10-10 09:32:16 +0200

    Do not encode json formatted body twice (#136)
    * ! do not encode already encoded things
    
    * ~ target version 9.4.3
    
    * ! json can be hash or array

---
+++ spec/request/body_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe LHC::Request do
+  context 'encode body' do
+    let(:encoded_data) { data.to_json }
+
+    before do
+      stub_request(:post, "http://datastore/q")
+        .with(body: encoded_data)
+        .to_return(status: 200)
+    end
+
+    context 'hash' do
+      let(:data) { { name: 'Steve' } }
+
+      it 'encodes the request body to the given format' do
+        LHC.post('http://datastore/q', body: data)
+      end
+
+      it 'does not encode the request body if it is already a string' do
+        LHC.post('http://datastore/q', body: encoded_data)
+      end
+    end
+
+    context 'array' do
+      let(:data) { [{ name: 'Steve' }] }
+
+      it 'encodes the request body to the given format' do
+        LHC.post('http://datastore/q', body: data)
+      end
+
+      it 'does not encode the request body if it is already a string' do
+        LHC.post('http://datastore/q', body: encoded_data)
+      end
+    end
+  end
+end
