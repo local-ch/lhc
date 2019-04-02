fe1816931f4ec046423dca590073a29ff4db6111
spape <s@pape.io>
2019-04-02 11:03:58 +0200

    + source logging

--- spec/interceptors/logging/main_spec.rb
+++ spec/interceptors/logging/main_spec.rb
@@ -8,16 +8,31 @@ describe LHC::Logging do
   before(:each) do
     LHC.config.interceptors = [LHC::Logging]
     LHC::Logging.logger = logger
+    stub_request(:get, 'http://local.ch').to_return(status: 200)
   end
 
   it 'does log information before and after every request made with LHC' do
-    stub_request(:get, 'http://local.ch').to_return(status: 200)
     LHC.get('http://local.ch')
     expect(logger).to have_received(:info).once.with(
-      %r{Before LHC request<\d+> GET http://local.ch at \d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2} Params={} Headers={.*?}}
+      %r{Before LHC request <\d+> GET http://local.ch at \d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2} Params={} Headers={.*?}}
     )
     expect(logger).to have_received(:info).once.with(
-      %r{After LHC response for request<\d+> GET http://local.ch at \d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2} Time=0ms URL=http://local.ch:80/}
+      %r{After LHC response for request <\d+> GET http://local.ch at \d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2} Time=0ms URL=http://local.ch:80/}
     )
+  end
+
+  context 'source' do
+
+    let(:source) { '/Users/Sebastian/LHC/test.rb' }
+
+    it 'does log the source if provided as option' do
+      LHC.get('http://local.ch', { source: source })
+      expect(logger).to have_received(:info).once.with(
+        %r{Before LHC request <\d+> GET http://local.ch at \d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2} Params={} Headers={.*?} \nCalled from #{source}}
+      )
+      expect(logger).to have_received(:info).once.with(
+        %r{After LHC response for request <\d+> GET http://local.ch at \d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2} Time=0ms URL=http://local.ch:80/ \nCalled from #{source}}
+      )
+    end
   end
 end
