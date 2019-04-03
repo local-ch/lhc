62f75eded072d8f8fab7c76826faa96b99b7bc61
Cédric Wider <wider.cedric@gmail.com>
2019-04-03 16:05:39 +0200

    Update Charset and Content-Type Headers (#151)
    * Introduce Accept-Charset header
    
    Instead of appendin the charset to the accept header, use the dedicated
    Accept-Charset header which takes precedence over the appended charset
    part in the Accept header.
    
    * Support json:api content-type
    
    json:api is a content-type that some APIs support. It is documented in
    great detail here: https://jsonapi.org/
    
    LHC/LHS can handle both content types and can safely state so in the
    accept header.
    
    * Bundle update
    
    * Update Version
    
    Updating just a minor version as suggested by @spape in #151

--- spec/response/data_accessor_spec.rb
+++ spec/response/data_accessor_spec.rb
@@ -6,7 +6,11 @@ describe LHC do
   context 'data accessor (hash with indifferent access)' do
     before(:each) do
       stub_request(:get, "http://local.ch/")
-        .with(headers: { 'Accept' => 'application/json; charset=utf-8', 'Content-Type' => 'application/json; charset=utf-8' })
+        .with(headers: {
+                'Accept' => 'application/json,application/vnd.api+json',
+                'Content-Type' => 'application/json; charset=utf-8',
+                'Accept-Charset' => 'utf-8'
+              })
         .to_return(body: { 'MyProp' => 'MyValue' }.to_json)
     end
 
