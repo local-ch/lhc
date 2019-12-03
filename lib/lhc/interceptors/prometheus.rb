2a42c3a6302b1de13d3f10b0fa77c3cb7d5196c7
Tom Streller <tom.streller@localsearch.ch>
2019-12-03 09:02:36 +0100

    Improve prometheus monitoring

--- lib/lhc/interceptors/prometheus.rb
+++ lib/lhc/interceptors/prometheus.rb
@@ -14,6 +14,10 @@ class LHC::Prometheus < LHC::Interceptor
   end
 
   def self.times_key
+    [LHC::Prometheus.namespace, 'lhc_request_seconds'].join('_').to_sym
+  end
+
+  def self.deprecated_times_key
     [LHC::Prometheus.namespace, 'lhc_times'].join('_').to_sym
   end
 
@@ -21,21 +25,31 @@ class LHC::Prometheus < LHC::Interceptor
     super(request)
     return if LHC::Prometheus.registered || LHC::Prometheus.client.blank?
     LHC::Prometheus.client.registry.counter(LHC::Prometheus.request_key, 'Counter of all LHC requests.')
-    LHC::Prometheus.client.registry.histogram(LHC::Prometheus.times_key, 'Times for all LHC requests.')
+    LHC::Prometheus.client.registry.histogram(LHC::Prometheus.deprecated_times_key, 'Times for all LHC requests (Deprecated)')
+    LHC::Prometheus.client.registry.histogram(LHC::Prometheus.times_key, 'Request timings for all LHC requests in seconds.')
     LHC::Prometheus.registered = true
   end
 
   def after_response
     return if !LHC::Prometheus.registered || LHC::Prometheus.client.blank?
+
+    host = URI.parse(request.url).host
+
     LHC::Prometheus.client.registry
       .get(LHC::Prometheus.request_key)
       .increment(
         code: response.code,
         success: response.success?,
-        timeout: response.timeout?
+        timeout: response.timeout?,
+        host: host
       )
+
+    LHC::Prometheus.client.registry
+      .get(LHC::Prometheus.deprecated_times_key)
+      .observe({}, response.time_ms)
+
     LHC::Prometheus.client.registry
       .get(LHC::Prometheus.times_key)
-      .observe({}, response.time_ms)
+      .observe({ host: host }, response.time)
   end
 end
--- spec/interceptors/prometheus_spec.rb
+++ spec/interceptors/prometheus_spec.rb
@@ -19,7 +19,9 @@ describe LHC::Prometheus do
       expect(Prometheus::Client.registry).to receive(:counter).and_call_original.once
         .with(:test_app_lhc_requests, 'Counter of all LHC requests.')
       expect(Prometheus::Client.registry).to receive(:histogram).and_call_original.once
-        .with(:test_app_lhc_times, 'Times for all LHC requests.')
+        .with(:test_app_lhc_times, 'Times for all LHC requests (Deprecated)')
+      expect(Prometheus::Client.registry).to receive(:histogram).and_call_original.once
+        .with(:test_app_lhc_request_seconds, 'Request timings for all LHC requests in seconds.')
 
       LHC.get('http://local.ch')
       LHC.get('http://local.ch') # second request, registration should happen only once
@@ -28,22 +30,30 @@ describe LHC::Prometheus do
 
   context 'logging' do
     let(:requests_registry_double) { double('requests_registry_double') }
+    let(:deprecated_times_registry_double) { double('deprecated_times_registry_double') }
     let(:times_registry_double) { double('times_registry_double') }
 
     it 'logs monitoring information to the created registries' do
       expect(Prometheus::Client.registry).to receive(:get).and_return(requests_registry_double).once
         .with(:test_app_lhc_requests)
-      expect(Prometheus::Client.registry).to receive(:get).and_return(times_registry_double).once
+      expect(Prometheus::Client.registry).to receive(:get).and_return(deprecated_times_registry_double).once
         .with(:test_app_lhc_times)
+      expect(Prometheus::Client.registry).to receive(:get).and_return(times_registry_double).once
+        .with(:test_app_lhc_request_seconds)
 
       expect(requests_registry_double).to receive(:increment).once
         .with(
           code: 200,
           success: true,
-          timeout: false
+          timeout: false,
+          host: 'local.ch'
         )
-      expect(times_registry_double).to receive(:observe).once
+
+      expect(deprecated_times_registry_double).to receive(:observe).once
         .with({}, 0)
+
+      expect(times_registry_double).to receive(:observe).once
+        .with({ host: 'local.ch' }, 0)
 
       LHC.get('http://local.ch')
     end
