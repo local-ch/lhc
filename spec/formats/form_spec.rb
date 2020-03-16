2e2989348f080b99ee917047554e33e2242351ec
Sebastian Pape <s@pape.io>
2020-03-16 11:53:37 +0100

    improve test

--- spec/formats/form_spec.rb
+++ spec/formats/form_spec.rb
@@ -8,11 +8,12 @@ describe LHC do
   context 'form' do
 
     it 'formats requests to be application/x-www-form-urlencoded' do
-      stub_request(:post, 'http://local.ch/') do |request|
-        raise 'Body Format is wrong' unless request.body != 'client_id=1234&client_secret=4567&grant_type=client_credentials'
-        raise 'Content-Type header wrong' unless request.headers['Content-Type'] == 'application/x-www-form-urlencoded'
-      end.to_return(status: 200)
-      response = LHC.form.post(
+      stub = stub_request(:post, 'http://local.ch/')
+        .with(body: 'client_id=1234&client_secret=4567&grant_type=client_credentials')
+        .with(headers: { 'Content-Type': 'application/x-www-form-urlencoded' })
+        .to_return(status: 200)
+      
+      LHC.form.post(
         'http://local.ch',
         body: {
           client_id: '1234',
@@ -20,6 +21,8 @@ describe LHC do
           grant_type: 'client_credentials'
         }
       )
+
+      expect(stub).to have_been_requested
     end
   end
 end
