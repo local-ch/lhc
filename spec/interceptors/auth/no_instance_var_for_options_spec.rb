24ad7dc2080f197244d7fc8f396b34fe9c7915cc
Sebastian Pape <spape@users.noreply.github.com>
2020-07-30 15:21:13 +0200

    No instance variable for auth options (#182)
    * no instance variable to allow other inteceptors to set auth
    
    * 12.1.1
    
    * fix rubocop

---
+++ spec/interceptors/auth/no_instance_var_for_options_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe LHC::Auth do
+  before(:each) do
+    class AuthPrepInterceptor < LHC::Interceptor
+
+      def before_request
+        request.options[:auth] = { bearer: 'sometoken' }
+      end
+    end
+
+    LHC.config.interceptors = [AuthPrepInterceptor, LHC::Auth]
+  end
+
+  after do
+    LHC.config.reset
+  end
+
+  it 'does not use instance variables internally so that other interceptors can still change auth options' do
+    stub_request(:get, "http://local.ch/")
+      .with(headers: { 'Authorization' => 'Bearer sometoken' })
+      .to_return(status: 200)
+    LHC.get('http://local.ch')
+  end
+end
