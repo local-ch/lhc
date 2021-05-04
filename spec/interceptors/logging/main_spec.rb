3e625501e785b2d38d89398a99c0d51ab9586fd9
Franca Rast <franca.rast@local.ch>
2021-05-04 16:04:20 +0200

    ~ use `a_string_including` instead of complicated regex

--- spec/interceptors/logging/main_spec.rb
+++ spec/interceptors/logging/main_spec.rb
@@ -7,21 +7,20 @@ describe LHC::Logging do
 
   before(:each) do
     LHC.config.interceptors = [LHC::Logging]
-    LHC.config.scrubs[:params] << 'api_key'
-    LHC.config.scrubs[:headers] << 'private_key'
     LHC::Logging.logger = logger
     stub_request(:get, /http:\/\/local.ch.*/).to_return(status: 200)
   end
 
   it 'does log information before and after every request made with LHC' do
-    LHC.get('http://local.ch', params: { api_key: '123-abc' }, headers: { private_key: 'abc-123' })
+    LHC.get('http://local.ch')
     expect(logger).to have_received(:info).once.with(
-      %r{Before LHC request <\d+> GET http://local.ch at \d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2} Params={:api_key=>\"\[FILTERED\]\"} Headers={.*?:private_key=>\"\[FILTERED\]\"}}
+      %r{Before LHC request <\d+> GET http://local.ch at \d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2} Params={} Headers={.*?}}
     )
     expect(logger).to have_received(:info).once.with(
       %r{After LHC response for request <\d+> GET http://local.ch at \d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2} Time=0ms URL=http://local.ch:80/}
     )
   end
+
 
   context 'source' do
     let(:source) { '/Users/Sebastian/LHC/test.rb' }
@@ -33,6 +32,26 @@ describe LHC::Logging do
       )
       expect(logger).to have_received(:info).once.with(
         %r{After LHC response for request <\d+> GET http://local.ch at \d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2} Time=0ms URL=http://local.ch:80/ \nCalled from #{source}}
+      )
+    end
+  end
+
+  context 'sensitive data' do
+    before :each do
+      LHC.config.scrubs[:params] << 'api_key'
+      LHC.config.scrubs[:headers] << 'private_key'
+      LHC.get('http://local.ch', params: { api_key: '123-abc' }, headers: { private_key: 'abc-123' })
+    end
+
+    it 'does log not log sensitive params information before every request made with LHC' do
+      expect(logger).to have_received(:info).once.with(
+        a_string_including('Params={:api_key=>"[FILTERED]"}')
+      )
+    end
+
+    it 'does log not log sensitive headers information before every request made with LHC' do
+      expect(logger).to have_received(:info).once.with(
+        a_string_including(':private_key=>"[FILTERED]"')
       )
     end
   end
