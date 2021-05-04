a340734f3d2f94a0f95d977aa6672ab26ae53df0
Franca Rast <franca.rast@local.ch>
2021-05-04 14:34:12 +0200

    + test that sensitive data gets filtered within the logs

--- spec/interceptors/logging/main_spec.rb
+++ spec/interceptors/logging/main_spec.rb
@@ -7,14 +7,16 @@ describe LHC::Logging do
 
   before(:each) do
     LHC.config.interceptors = [LHC::Logging]
+    LHC.config.scrubs[:params] << 'api_key'
+    LHC.config.scrubs[:headers] << 'private_key'
     LHC::Logging.logger = logger
-    stub_request(:get, 'http://local.ch').to_return(status: 200)
+    stub_request(:get, /http:\/\/local.ch.*/).to_return(status: 200)
   end
 
   it 'does log information before and after every request made with LHC' do
-    LHC.get('http://local.ch')
+    LHC.get('http://local.ch', params: { api_key: '123-abc' }, headers: { private_key: 'abc-123' })
     expect(logger).to have_received(:info).once.with(
-      %r{Before LHC request <\d+> GET http://local.ch at \d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2} Params={} Headers={.*?}}
+      %r{Before LHC request <\d+> GET http://local.ch at \d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2} Params={:api_key=>\"\[FILTERED\]\"} Headers={.*?:private_key=>\"\[FILTERED\]\"}}
     )
     expect(logger).to have_received(:info).once.with(
       %r{After LHC response for request <\d+> GET http://local.ch at \d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2} Time=0ms URL=http://local.ch:80/}
